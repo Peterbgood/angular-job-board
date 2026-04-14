990358ba65a65d5f6f48832bbb4ffcc5862508fa
Peter Good <peterwgood77@gmail.com>
2026-04-14 13:11:13 -0400

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router'; // Import this
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, withHashLocation } from '@angular/router'; // Add withHashLocation
+import { provideHttpClient, withJsonpSupport } from '@angular/common/http';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withHashLocation()) // Add it here
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withHashLocation()), // Add the function here
+    provideHttpClient(withJsonpSupport())
   ]
 };
